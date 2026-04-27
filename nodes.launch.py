cbb4753660666f9005061e3de44c5f271aeafbd3
Drew Swinney <drewswinney@gmail.com>
2026-04-27 17:30:14 -0400

    Deploy code for Polyrover

--- nodes.launch.py
+++ nodes.launch.py
@@ -11,7 +11,7 @@ def generate_launch_description():
             output="screen",
             additional_env={
                 "POLYFLOW_NODE_ID": "69ee7285cfbebaa7a6f6f9c9",
-                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"motor_id":"69ee7500f25c6f3e7bc9e6ce","mode":"speed","max_speed":1,"timeout_s":0}')),
+                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"motor_id":"69ee7500f25c6f3e7bc9e6ce","mode":"speed","max_speed":360,"timeout_s":0}')),
                 "POLYFLOW_CONFIGURATION": json.dumps(json.loads('{"namespace":null,"rate_hz":50,"lifecycle":null}')),
                 "POLYFLOW_PINS": json.dumps(json.loads('[{"pin_id":"69a3cd62b3f393789aad70a2:command","name":"command","direction":"input","msg_type":"std_msgs/Float64"}]')),
                 "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(json.loads('[{"connection_id":"69ee79fadf065b5cb52a56b9","source_node_id":"69ee79f5df065b5cb52a5591","source_pin_id":"rear_left_motor","target_pin_id":"command"}]')),
@@ -25,7 +25,7 @@ def generate_launch_description():
             output="screen",
             additional_env={
                 "POLYFLOW_NODE_ID": "69ee7287cfbebaa7a6f6fa37",
-                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"motor_id":"69ee758cf25c6f3e7bc9f1b6","mode":"speed","max_speed":1,"timeout_s":0}')),
+                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"motor_id":"69ee758cf25c6f3e7bc9f1b6","mode":"speed","max_speed":360,"timeout_s":0}')),
                 "POLYFLOW_CONFIGURATION": json.dumps(json.loads('{"namespace":null,"rate_hz":50,"lifecycle":null}')),
                 "POLYFLOW_PINS": json.dumps(json.loads('[{"pin_id":"69a3cd62b3f393789aad70a2:command","name":"command","direction":"input","msg_type":"std_msgs/Float64"}]')),
                 "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(json.loads('[{"connection_id":"69ee79f8df065b5cb52a564d","source_node_id":"69ee79f5df065b5cb52a5591","source_pin_id":"front_left_motor","target_pin_id":"command"}]')),
@@ -39,7 +39,7 @@ def generate_launch_description():
             output="screen",
             additional_env={
                 "POLYFLOW_NODE_ID": "69ee7289cfbebaa7a6f6faa5",
-                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"motor_id":"69ee74f9f25c6f3e7bc9e5c6","mode":"speed","max_speed":1,"timeout_s":0}')),
+                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"motor_id":"69ee74f9f25c6f3e7bc9e5c6","mode":"speed","max_speed":360,"timeout_s":0}')),
                 "POLYFLOW_CONFIGURATION": json.dumps(json.loads('{"namespace":null,"rate_hz":50,"lifecycle":null}')),
                 "POLYFLOW_PINS": json.dumps(json.loads('[{"pin_id":"69a3cd62b3f393789aad70a2:command","name":"command","direction":"input","msg_type":"std_msgs/Float64"}]')),
                 "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(json.loads('[{"connection_id":"69ee79fddf065b5cb52a5725","source_node_id":"69ee79f5df065b5cb52a5591","source_pin_id":"front_right_motor","target_pin_id":"command"}]')),
@@ -53,7 +53,7 @@ def generate_launch_description():
             output="screen",
             additional_env={
                 "POLYFLOW_NODE_ID": "69ee728ccfbebaa7a6f6fb13",
-                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"motor_id":"69ee7913df065b5cb52a51d1","mode":"speed","max_speed":1,"timeout_s":0}')),
+                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"motor_id":"69ee7913df065b5cb52a51d1","mode":"speed","max_speed":360,"timeout_s":0}')),
                 "POLYFLOW_CONFIGURATION": json.dumps(json.loads('{"namespace":null,"rate_hz":50,"lifecycle":null}')),
                 "POLYFLOW_PINS": json.dumps(json.loads('[{"pin_id":"69a3cd62b3f393789aad70a2:command","name":"command","direction":"input","msg_type":"std_msgs/Float64"}]')),
                 "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(json.loads('[{"connection_id":"69ee79ffdf065b5cb52a5791","source_node_id":"69ee79f5df065b5cb52a5591","source_pin_id":"rear_right_motor","target_pin_id":"command"}]')),
@@ -67,7 +67,7 @@ def generate_launch_description():
             output="screen",
             additional_env={
                 "POLYFLOW_NODE_ID": "69ee79f5df065b5cb52a5591",
-                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"wheel_radius":0.05,"wheel_separation":0.3,"max_wheel_speed":1}')),
+                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"wheel_radius":0.95,"wheel_separation":0.225,"max_wheel_speed":1}')),
                 "POLYFLOW_CONFIGURATION": json.dumps(json.loads('{"namespace":null,"rate_hz":50,"lifecycle":null}')),
                 "POLYFLOW_PINS": json.dumps(json.loads('[{"pin_id":"69a3c8e4b3f393789aad6f84:cmd_vel","name":"cmd_vel","direction":"input","msg_type":"geometry_msgs/Twist"},{"pin_id":"69a3c8e4b3f393789aad6f84:mode","name":"mode","direction":"input","msg_type":"polyflow_msgs/ModeState"},{"pin_id":"69a3c8e4b3f393789aad6f84:front_left_motor","name":"front_left_motor","direction":"output","msg_type":"std_msgs/Float64"},{"pin_id":"69a3c8e4b3f393789aad6f84:rear_left_motor","name":"rear_left_motor","direction":"output","msg_type":"std_msgs/Float64"},{"pin_id":"69a3c8e4b3f393789aad6f84:front_right_motor","name":"front_right_motor","direction":"output","msg_type":"std_msgs/Float64"},{"pin_id":"69a3c8e4b3f393789aad6f84:rear_right_motor","name":"rear_right_motor","direction":"output","msg_type":"std_msgs/Float64"}]')),
                 "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(json.loads('[{"connection_id":"69ee7a38df065b5cb52a5a4b","source_node_id":"69ee7a34df065b5cb52a598f","source_pin_id":"cmd_vel","target_pin_id":"cmd_vel"},{"connection_id":"69ee7a44df065b5cb52a5bc5","source_node_id":"69ee7a41df065b5cb52a5b09","source_pin_id":"mode","target_pin_id":"mode"}]')),
@@ -81,7 +81,7 @@ def generate_launch_description():
             output="screen",
             additional_env={
                 "POLYFLOW_NODE_ID": "69ee7a34df065b5cb52a598f",
-                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"device_index":0,"poll_rate_hz":60,"deadzone":0.05,"max_linear_speed":1,"max_angular_speed":2}')),
+                "POLYFLOW_PARAMETERS": json.dumps(json.loads('{"device_index":0,"poll_rate_hz":66,"deadzone":0.05,"max_linear_speed":1,"max_angular_speed":2,"output_mode":"diff_drive"}')),
                 "POLYFLOW_CONFIGURATION": json.dumps(json.loads('{"namespace":null,"rate_hz":60,"lifecycle":null}')),
                 "POLYFLOW_PINS": json.dumps(json.loads('[{"pin_id":"69a3c77bb3f393789aad6f62:axes","name":"axes","direction":"output","msg_type":"polyflow_msgs/GamepadAxes"},{"pin_id":"69a3c77bb3f393789aad6f62:buttons","name":"buttons","direction":"output","msg_type":"polyflow_msgs/GamepadButtons"},{"pin_id":"69a3c77bb3f393789aad6f62:cmd_vel","name":"cmd_vel","direction":"output","msg_type":"geometry_msgs/Twist"}]')),
                 "POLYFLOW_INBOUND_CONNECTIONS": json.dumps(json.loads('[]')),
